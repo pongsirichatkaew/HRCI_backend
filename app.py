3b5441de3280cd5f4cd7e677b5ba1b8fdcbd18cb
DESKTOP-0AFILKM <Black_Sky_light@hotmail.com>
2018-07-16 11:35:10 +0700

    login

--- app.py
+++ app.py
@@ -1,46 +1,33 @@
 from dbConfig import *
 from test import *
 
-@app.route('/hello/<firstname>/<lastname>', methods=['GET'])
-@connect_sql()
-def hello(cursor, firstname, lastname):
-    sql = "SELECT * FROM test WHERE firstname=%s AND lastname=%s"
-    cursor.execute(sql,(firstname, lastname))
+@app.route('/hello', methods=['GET'])
+def hello():
+    return 'hello'
+
+@app.route('/login', methods=['POST'])
+@connect_sql2()
+def login(cursor):
+    _data = request.json
+    username = _data['username']
+    password = _data['password']
+    sql = "SELECT * FROM user WHERE username = %s and password = %s"
+    conn = mysql2.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, (username, hashlib.sha512(password).hexdigest()))
+    data = cursor.fetchall()
     columns = [column[0] for column in cursor.description]
-    result = toJson(cursor.fetchall(),columns)
+    _output = toJson(data, columns)
+
+    result = {}
+    if len(_output) != 0:
+        result['message'] = 'login success'
+        result['userid'] = _output[0]['userid']
+        result['username'] = _output[0]['username']
+    else:
+        result['message'] = 'login fail'
     return jsonify(result)
 
 
-@app.route('/getName', methods=['POST'])
-@connect_sql()
-def getName(cursor):
-    data = request.json
-    sql = "SELECT * FROM test WHERE firstname=%s AND lastname=%s"
-    cursor.execute(sql,(data['firstname'], data['lastname']))
-    columns = [column[0] for column in cursor.description]
-    result = toJson(cursor.fetchall(),columns)
-    return jsonify(result)
-
-@app.route('/hello2', methods=['GET'])
-def hello2():
-    return jsonify(getData('hello'))
-
-@connect_sql()
-def getData(c, data):
-    print data
-    c.execute("SELECT * FROM test")
-    columns = [column[0] for column in c.description]
-    result = toJson(c.fetchall(),columns)
-    return result
-
-@app.route('/insertdata/<firstname>/<lastname>', methods=['GET'])
-@connect_sql()
-def InsertData(cursor,firstname, lastname):
-    sql = """
-    INSERT INTO test(firstname, lastname)
-    VALUES (%s,%s)
-    """
-    cursor.execute(sql, (firstname, lastname))
-    return 'success'
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',threaded=True,port=5000)
--- dbConfig.py
+++ dbConfig.py
@@ -5,6 +5,13 @@ from flask_cors import CORS, cross_origin
 from functools import wraps
 from flaskext.mysql import MySQL
 
+import hashlib
+import base64
+import datetime
+from datetime import datetime
+import string
+import random
+
 
 app = Flask(__name__)
 CORS(app)
@@ -15,6 +22,14 @@ app.config['MYSQL_DATABASE_DB'] = 'HRCI'
 app.config['MYSQL_DATABASE_HOST'] = '203.154.58.87'
 mysql = MySQL()
 mysql.init_app(app)
+
+app2 = Flask(__name__)
+app2.config['MYSQL_DATABASE_USER'] = "root"
+app2.config['MYSQL_DATABASE_PASSWORD'] = "l^9i@xib,kIlkily,ryoTN"
+app2.config['MYSQL_DATABASE_DB'] = 'intranetdb'
+app2.config['MYSQL_DATABASE_HOST'] = '203.150.57.159'
+mysql2 = MySQL()
+mysql2.init_app(app2)
 
 
 def connect_sql():
@@ -33,9 +48,42 @@ def connect_sql():
             return return_val
         return wrapper
     return wrap
-
+def connect_sql2():
+        def wrap(fn):
+            @wraps(fn)
+            def wrapper(*args, **kwargs):
+                try:
+                    # Setup connection
+                    connection = mysql2.connect()
+                    cursor = connection.cursor()
+                    return_val = fn(cursor, *args, **kwargs)
+                finally:
+                    # Close connection
+                    connection.commit()
+                    connection.close()
+                return return_val
+            return wrapper
+        return wrap
 def toJson(data,columns):
     results = []
     for row in data:
         results.append(dict(zip(columns, row)))
     return results
+def logserver(msg):
+    current_app.logger.info(msg)
+def decode(data):
+    return base64.b64decode(data[:-5][::-1])
+
+def encode(data):
+    return (base64.b64encode(str(data)))[::-1] + id_generator()
+
+def logserver(message):
+    current_app.logger.info(message)
+
+def id_generator():
+    size = 5
+    chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    return ''.join(random.choice(chars) for _ in range(size))
+def converterDate (output):
+    if isinstance(output, datetime.date):
+        return output.__str__()
