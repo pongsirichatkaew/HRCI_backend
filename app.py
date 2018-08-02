64a90b1cafa7348f5c803677e52d0854a53f7262
DESKTOP-0AFILKM <Black_Sky_light@hotmail.com>
2018-08-02 21:19:43 +0700

    Revert "Qry_Criminal"
    This reverts commit e5d83e459aee97600420651c2054f9361b1ad3db.

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from status.employee_Deputy_Manager_Hr import*
 from status.employee_Deputy_Manager_PayRoll import*
 from status.Status import *
 from employee.Employee import *
-from employee.criminal import *
 from Appform.appform import *
 
 @app.route('/hello', methods=['GET'])
