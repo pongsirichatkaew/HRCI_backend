7c84f4d235f14acedd13fae54eba9b8a38b0bde4
DESKTOP-0AFILKM <Black_Sky_light@hotmail.com>
2018-08-14 10:04:40 +0700

    status_Admin

---
+++ status/Admin.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from dbConfig import *
+
+@app.route('/InsertAdmin', methods=['POST'])
+@connect_sql()
+def InsertAdmin(cursor):
+    try:
+        data = request.json
+        source = data['source']
+        data_new = source
+        org_name_id = data_new['org_name_id']
+        org_name_detail = data_new['org_name_detail']
+        email = data_new['email']
+        validstatus = data_new['validstatus']
+        sql = "INSERT INTO org_name (org_name_id,org_name_detail,email,validstatus) VALUES (%s,%s,%s,%s)"
+        cursor.execute(sql,(org_name_id,org_name_detail,email,validstatus))
+        return "success"
+    except Exception as e:
+        logserver(e)
+        return "fail"
+@app.route('/EditAdmin', methods=['POST'])
+@connect_sql()
+def EditAdmin(cursor):
+    try:
+        data = request.json
+        source = data['source']
+        data_new = source
+        id = data_new['id']
+        org_name_id = data_new['org_name_id']
+        org_name_detail = data_new['org_name_detail']
+        email = data_new['email']
+        validstatus = data_new['validstatus']
+        sqlUp = "UPDATE org_name SET org_name_id=%s,org_name_detail=%s,email=%s,validstatus=%s WHERE id=%s"
+        cursor.execute(sqlUp,(org_name_id,org_name_detail,email,validstatus,id))
+        # sqlIn = "INSERT INTO org_name (org_name_id,org_name_detail,email) VALUES (%s,%s,%s)"
+        # cursor.execute(sqlIn,(org_name_id,org_name_detail,email))
+        return "success"
+    except Exception as e:
+        logserver(e)
+        return "fail"
+@app.route('/QryAdmin', methods=['POST'])
+@connect_sql()
+def QryAdmin(cursor):
+    try:
+        sql = "SELECT org_name_id,org_name_detail,id,email,validstatus FROM org_name"
+        cursor.execute(sql)
+        columns = [column[0] for column in cursor.description]
+        result = toJson(cursor.fetchall(),columns)
+        return jsonify(result)
+    except Exception as e:
+        logserver(e)
+        return "fail"
