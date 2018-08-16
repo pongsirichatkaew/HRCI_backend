1749213ddd148414c7710664be5428dec40b1f65
rainbow <bowbeau32@gmail.com>
2018-08-16 14:01:00 +0700

    img base64

--- app.py
+++ app.py
@@ -20,10 +20,12 @@ def hello():
 
 @app.route('/testbase64', methods=['POST'])
 def testbase64():
-      data = base64.b64encode('http://career.inet.co.th/assets/attachment/2018062800691/2018062800691_siriporn.jpg')
-      print(data)
-      return jsonify(data)
-
+    # with open('https://www.kroger.com/product/images/medium/front/0000000003283', 'rb') as image_file:
+    #     encoded_string = base64.b64encode(image_file.read())
+    # return jsonify(encoded_string)
+    with open('C:\\work\\HRCI_Fornt\\static\\img\\14434602881204.png', 'rb') as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    return jsonify(encoded_string)
 @app.route('/TestgenEM', methods=['POST'])
 def TestgenEM():
     try:
