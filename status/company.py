c91e2d6ad225ebf23c5d407210d11665d1c994d3
DESKTOP-0AFILKM <Black_Sky_light@hotmail.com>
2018-08-22 11:20:26 +0700

    edit_upload_company

--- status/company.py
+++ status/company.py
@@ -5,47 +5,50 @@ from dbConfig import *
 @app.route('/InsertCompany', methods=['POST'])
 @connect_sql()
 def InsertCompany(cursor):
-    # try:
-    sqlQry = "SELECT companyid FROM company ORDER BY companyid DESC LIMIT 1"
-    cursor.execute(sqlQry)
-    columns = [column[0] for column in cursor.description]
-    result = toJson(cursor.fetchall(),columns)
-    companyid_last=str(result[0]['companyid']+1)
+    try:
+        sqlQry = "SELECT companyid FROM company ORDER BY companyid DESC LIMIT 1"
+        cursor.execute(sqlQry)
+        columns = [column[0] for column in cursor.description]
+        result = toJson(cursor.fetchall(),columns)
+        companyid_last=str(result[0]['companyid']+1)
 
-    currentTime = datetime.today().strftime('%Y%m%d%H%M%S%f')
-    path = 'uploads/' + companyid_last
-    if not os.path.exists(path):
-        os.makedirs(path)
-    if request.method == 'POST':
-        file = request.files['file']
-    if file:
-        file.save(os.path.join(path, currentTime + '_company_img.png'))
-        path_image = path+'/'+currentTime+'_company_img.png'
-    else:
-      return 'file is not allowed'
-
-    sql = "INSERT INTO company(acronym,companyid,companyname,company_short_name,phone,email,address_company,imageName,createby) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-    cursor.execute(sql,(request.form['acronym'],companyid_last,request.form['companyname'],request.form['company_short_name'],request.form['phone'],request.form['email'],request.form['address_company'],path_image,request.form['createby']))
-
-        # sql = "INSERT INTO company(acronym,companyid,companyname,company_short_name,phone,email,address_company,imageName,createby) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
-        # cursor.execute(sql,(request.form['acronym'],companyid_last,request.form['companyname'],request.form['company_short_name'],request.form['phone'],request.form['email'],request.form['address_company'],path_image,request.form['createby']))
-    return "success"
-    # except Exception as e:
-    #     logserver(e)
-    #     return "fail"
+        currentTime = datetime.today().strftime('%Y%m%d%H%M%S%f')
+        path = 'uploads/' + companyid_last
+        if not os.path.exists(path):
+            os.makedirs(path)
+        if request.method == 'POST':
+            file = request.files['file']
+        if file:
+            file.save(os.path.join(path, currentTime + '_company_img.png'))
+            path_image = path+'/'+currentTime+'_company_img.png'
+        else:
+            return 'file is not allowed'
+        sql = "INSERT INTO company(acronym,companyid,companyname,company_short_name,phone,email,address_company,imageName,createby) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        cursor.execute(sql,(request.form['acronym'],companyid_last,request.form['companyname'],request.form['company_short_name'],request.form['phone'],request.form['email'],request.form['address_company'],path_image,request.form['createby']))
+        return "success"
+    except Exception as e:
+        logserver(e)
+        return "fail"
 @app.route('/EditCompany', methods=['POST'])
 @connect_sql()
 def EditCompany(cursor):
     try:
-        data = request.json
-        source = data['source']
-        data_new = source
+        sqlUp = "UPDATE company SET validstatus=0,createby=%s WHERE companyid=%s"
+        cursor.execute(sqlUp,(request.form['createby'],request.form['companyid']))
 
-        sqlUp = "UPDATE company SET validstatus= 0 WHERE companyid=%s"
-        cursor.execute(sqlUp,(data_new['companyid']))
-
-        sqlIn = "INSERT INTO company(acronym,companyid,companyname,company_short_name,phone,email,address_company,imageName) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
-        cursor.execute(sqlIn,(data_new['acronym'],data_new['companyid'],data_new['companyname'],data_new['company_short_name'],data_new['phone'],data_new['email'],data_new['address_company'],data_new['imageName']))
+        currentTime = datetime.today().strftime('%Y%m%d%H%M%S%f')
+        path = 'uploads/' + companyid_last
+        if not os.path.exists(path):
+            os.makedirs(path)
+        if request.method == 'POST':
+            file = request.files['file']
+        if file:
+            file.save(os.path.join(path, currentTime + '_company_img.png'))
+            path_image = path+'/'+currentTime+'_company_img.png'
+        else:
+            return 'file is not allowed'
+        sql = "INSERT INTO company(acronym,companyid,companyname,company_short_name,phone,email,address_company,imageName,createby) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        cursor.execute(sql,(request.form['acronym'],companyid_last,request.form['companyname'],request.form['company_short_name'],request.form['phone'],request.form['email'],request.form['address_company'],path_image,request.form['createby']))
         return "success"
     except Exception as e:
         logserver(e)
@@ -54,7 +57,7 @@ def EditCompany(cursor):
 @connect_sql()
 def QryCompany(cursor):
     try:
-        sql = "SELECT id,companyid,companyname,company_short_name,email,address_company,imageName,phone,validstatus,acronym FROM company WHERE validstatus =1"
+        sql = "SELECT companyid,companyname,company_short_name,email,address_company,imageName,phone,validstatus,acronym FROM company WHERE validstatus =1"
         cursor.execute(sql)
         columns = [column[0] for column in cursor.description]
         result = toJson(cursor.fetchall(),columns)
@@ -70,11 +73,11 @@ def DeleteCompany(cursor):
         source = dataInput['source']
         data_new = source
 
-        sql_OldTimeCompany = "UPDATE company SET validstatus=0 WHERE companyid=%s"
-        cursor.execute(sql_OldTimeCompany,(data_new['companyid']))
+        sql_OldTimeCompany = "UPDATE company SET validstatus=0,createby=%s WHERE companyid=%s"
+        cursor.execute(sql_OldTimeCompany,(data_new['createby'],data_new['companyid']))
 
-        sql_NewTimeCompany = "INSERT INTO company(acronym,companyid,companyname,company_short_name,phone,email,address_company,imageName,validstatus) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        cursor.execute(sql_NewTimeCompany,(data_new['acronym'],data_new['companyid'],data_new['companyname'],data_new['company_short_name'],data_new['phone'],data_new['email'],data_new['address_company'],data_new['imageName'],0))
+        sql_NewTimeCompany = "INSERT INTO company(acronym,companyid,companyname,company_short_name,phone,email,address_company,imageName,createby,validstatus) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        cursor.execute(sql_NewTimeCompany,(data_new['acronym'],data_new['companyid'],data_new['companyname'],data_new['company_short_name'],data_new['phone'],data_new['email'],data_new['address_company'],data_new['imageName'],data_new['createby'],0))
         return "success"
     except Exception as e:
         logserver(e)
