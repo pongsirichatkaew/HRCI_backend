e02eb69d64f1690df9bf03df36d3ca2f2f2ec7be
DESKTOP-0AFILKM <Black_Sky_light@hotmail.com>
2018-08-22 09:32:56 +0700

    test_upload_db

--- status/company.py
+++ status/company.py
@@ -6,59 +6,30 @@ from dbConfig import *
 @connect_sql()
 def InsertCompany(cursor):
     try:
-        # data = request.json
-        # source = data['source']
-        # data_new = source
+        sqlQry = "SELECT companyid FROM company ORDER BY companyid DESC LIMIT 1"
+        cursor.execute(sqlQry)
+        columns = [column[0] for column in cursor.description]
+        result = toJson(cursor.fetchall(),columns)
+        companyid_last=result[0]['companyid']+1
 
         currentTime = datetime.today().strftime('%Y%m%d%H%M%S%f')
-        path = 'uploads/' + request.form['userId']
+        path = 'uploads/' + companyid_last
         if not os.path.exists(path):
             os.makedirs(path)
         if request.method == 'POST':
             file = request.files['file']
         if file:
-    	  file.save(os.path.join(path, currentTime + '_profile_img.png'))
-          userLogSaveFile(request.form['userId'], currentTime + '_profile_img.png')
+    	  file.save(os.path.join(path, currentTime + '_company_img.png'))
+          path_image = str(path+currentTime+'_company_img.png')
         else:
           return 'file is not allowed'
 
-        # sqlQry = "SELECT companyid FROM company ORDER BY companyid DESC LIMIT 1"
-        # cursor.execute(sqlQry)
-        # columns = [column[0] for column in cursor.description]
-        # result = toJson(cursor.fetchall(),columns)
-        # companyid_last=result[0]['companyid']+1
-
-        # sql = "INSERT INTO company(acronym,companyid,companyname,company_short_name,phone,email,address_company,imageName) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
-        # cursor.execute(sql,(data_new['acronym'],companyid_last,data_new['companyname'],data_new['company_short_name'],data_new['phone'],data_new['email'],data_new['address_company'],data_new['imageName']))
+        sql = "INSERT INTO company(acronym,companyid,companyname,company_short_name,phone,email,address_company,imageName,createby) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+        cursor.execute(sql,(request.form['acronym'],companyid_last,request.form['companyname'],request.form['company_short_name'],request.form['phone'],request.form['email'],request.form['address_company'],path_image,request.form['createby']))
         return "success"
     except Exception as e:
         logserver(e)
         return "fail"
-        # dataInput = request.json
-        # sqlQry = "SELECT companyid FROM company ORDER BY companyid DESC LIMIT 1"
-        # cursor3.execute(sqlQry)
-        # columns = [column[0] for column in cursor3.description]
-        # result = toJson(cursor3.fetchall(),columns)
-        # company_id_last=result[0]['companyid']+1
-
-        # currentTime = datetime.datetime.today().strftime('%Y%m%d%H%M%S%f')
-        # path = 'uploads/users/' + request.form['userId']
-        # if not os.path.exists(path):
-        #     os.makedirs(path)
-        # if request.method == 'POST':
-        #     file = request.files['file']
-        # if file:
-    	#   file.save(os.path.join(path, currentTime + '_profile_img.png'))
-        #   userLogSaveFile(request.form['userId'], currentTime + '_profile_img.png')
-        # else:
-        #   return 'file is not allowed'
-
-        # sql = "INSERT INTO company (companyid,acronym,companyname,company_short_name,email,address_company,phone,imageName) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
-        # cursor.execute(sql,(company_id_last,result['acronym'],result['companyname'],result['company_short_name'],result['email'],result['address_company'],result['phone'],imageName))
-    #     return "success"
-    # except Exception as e:
-    #     logserver(e)
-    #     return "fail"
 @app.route('/EditCompany', methods=['POST'])
 @connect_sql()
 def EditCompany(cursor):
