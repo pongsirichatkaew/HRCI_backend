ccd32f00adb16663c45b848343001368a2baea1b
Jirakit_Dan <Black_Sky_light@hotmail.com>
2018-09-26 10:12:21 +0700

    ดัก_QRY_export_excelกรณีไม่มีข้อมูล

--- app/employee/criminal.py
+++ app/employee/criminal.py
@@ -186,28 +186,31 @@ def ExportToExcel(cursor):
         data_new = source
         year=str(data_new['year'])
         month=str(data_new['month'])
-
-        sql4 ="""SELECT Personal.*,Address.AddressType, Address.HouseNo, Address.Street, Address.DISTRICT_ID, Address.AMPHUR_ID, Address.PROVINCE_ID, Address.PostCode, Address.Tel, Address.Fax,homeTable.AddressType as homeAddress, homeTable.HouseNo as homeHouseNo, homeTable.Street as homeStreet,employee.company_id as companyid,employee.start_work as start_work,position.position_detail as position_detail, \
-        section.sect_detail as sect_detail,org_name.org_name_detail as org_name_detail,cost_center_name.cost_detail as cost_detail,company.company_short_name,
-        homeTable.DISTRICT_ID as homeDistrict, homeTable.AMPHUR_ID as homeAmphur, homeTable.PROVINCE_ID as homeProvince, homeTable.PostCode as homePostCode, homeTable.Tel as homeTel, homeTable.Fax as homeFax,employee.create_at as timedate,
-        Family.Name as fatherName, Family.Surname as fatherSurname,motherTable.Name as motherName, motherTable.Surname as motherSurname
-        FROM Personal
-        LEFT JOIN employee ON employee.citizenid = Personal.ID_CardNo
-        LEFT JOIN position ON position.position_id = employee.position_id
-        LEFT JOIN section ON section.sect_id = employee.section_id
-        LEFT JOIN org_name ON org_name.org_name_id = employee.org_name_id
-        LEFT JOIN cost_center_name ON cost_center_name.cost_center_name_id = employee.cost_center_name_id
-        LEFT JOIN company ON company.companyid = employee.company_id
-        LEFT JOIN Address ON Address.ID_CardNo = Personal.ID_CardNo
-        LEFT JOIN Family ON Family.ID_CardNo = Personal.ID_CardNo \
-        LEFT JOIN (SELECT * FROM Address WHERE AddressType = 'Home') AS homeTable ON homeTable.ID_CardNo = Personal.ID_CardNo
-        LEFT JOIN (SELECT * FROM Family WHERE MemberType = 'Mother') AS motherTable ON motherTable.ID_CardNo = Personal.ID_CardNo
-        WHERE Address.AddressType = 'Present' AND Family.MemberType = 'Father' AND Personal.validstatus=1 AND Address.validstatus=1 AND Family.validstatus=1 AND position.validstatus=1 AND \
-        section.validstatus=1 AND org_name.validstatus=1 AND cost_center_name.validstatus=1 AND company.validstatus=1 AND \
-        employee.create_at LIKE '""" + year + """-""" + month + """%'"""
-        cursor.execute(sql4)
-        columns = [column[0] for column in cursor.description]
-        result = toJson(cursor.fetchall(),columns)
+        try:
+            sql4 ="""SELECT Personal.*,Address.AddressType, Address.HouseNo, Address.Street, Address.DISTRICT_ID, Address.AMPHUR_ID, Address.PROVINCE_ID, Address.PostCode, Address.Tel, Address.Fax,homeTable.AddressType as homeAddress, homeTable.HouseNo as homeHouseNo, homeTable.Street as homeStreet,employee.company_id as companyid,employee.start_work as start_work,position.position_detail as position_detail, \
+            section.sect_detail as sect_detail,org_name.org_name_detail as org_name_detail,cost_center_name.cost_detail as cost_detail,company.company_short_name,
+            homeTable.DISTRICT_ID as homeDistrict, homeTable.AMPHUR_ID as homeAmphur, homeTable.PROVINCE_ID as homeProvince, homeTable.PostCode as homePostCode, homeTable.Tel as homeTel, homeTable.Fax as homeFax,employee.create_at as timedate,
+            Family.Name as fatherName, Family.Surname as fatherSurname,motherTable.Name as motherName, motherTable.Surname as motherSurname
+            FROM Personal
+            LEFT JOIN employee ON employee.citizenid = Personal.ID_CardNo
+            LEFT JOIN position ON position.position_id = employee.position_id
+            LEFT JOIN section ON section.sect_id = employee.section_id
+            LEFT JOIN org_name ON org_name.org_name_id = employee.org_name_id
+            LEFT JOIN cost_center_name ON cost_center_name.cost_center_name_id = employee.cost_center_name_id
+            LEFT JOIN company ON company.companyid = employee.company_id
+            LEFT JOIN Address ON Address.ID_CardNo = Personal.ID_CardNo
+            LEFT JOIN Family ON Family.ID_CardNo = Personal.ID_CardNo \
+            LEFT JOIN (SELECT * FROM Address WHERE AddressType = 'Home') AS homeTable ON homeTable.ID_CardNo = Personal.ID_CardNo
+            LEFT JOIN (SELECT * FROM Family WHERE MemberType = 'Mother') AS motherTable ON motherTable.ID_CardNo = Personal.ID_CardNo
+            WHERE Address.AddressType = 'Present' AND Family.MemberType = 'Father' AND Personal.validstatus=1 AND Address.validstatus=1 AND Family.validstatus=1 AND position.validstatus=1 AND \
+            section.validstatus=1 AND org_name.validstatus=1 AND cost_center_name.validstatus=1 AND company.validstatus=1 AND \
+            employee.create_at LIKE '""" + year + """-""" + month + """%'"""
+            cursor.execute(sql4)
+            columns = [column[0] for column in cursor.description]
+            result = toJson(cursor.fetchall(),columns)
+        except Exception as e:
+            logserver(e)
+            return "No_Data"
 
         isSuccess = True
         reasonCode = 200
