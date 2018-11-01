6277a96e95727f84bf6b8a757b1b6791eb18671f
remiixz <mixza_naruk@hotmail.com>
2018-11-01 09:42:10 +0700

    signature back

--- app/status/signature_crime.py
+++ app/status/signature_crime.py
@@ -10,18 +10,18 @@ def InsertSignature_crime(cursor):
         source = dataInput['source']
         data_new = source
         sqlQry = "SELECT name_signature_crime_id FROM signature_crime ORDER BY name_signature_crime_id DESC LIMIT 1"
-        cursor3.execute(sqlQry)
-        columns = [column[0] for column in cursor3.description]
-        result = toJson(cursor3.fetchall(),columns)
+        cursor.execute(sqlQry)
+        columns = [column[0] for column in cursor.description]
+        result = toJson(cursor.fetchall(),columns)
         name_signature_crime_id_last=result[0]['name_signature_crime_id']+1
 
-        sql = "INSERT INTO signature_crime (name_signature_crime_id,name_signature_crime,surname_signature_crime,position_signature_crime,createby) VALUES (%s,%s,%s,%s,%s)"
-        cursor3.execute(sql,(name_signature_crime_id_last,data_new['name_signature_crime'],data_new['surname_signature_crime'],data_new['position_signature_crime'],data_new['createby']))
+        sql = "INSERT INTO signature_crime (employeeid,name_signature_crime_id,name_signature_crime,surname_signature_crime,position_id,createby) VALUES (%s,%s,%s,%s,%s,%s)"
+        cursor.execute(sql,(data_new['employeeid'],name_signature_crime_id_last,data_new['name_signature_crime'],data_new['surname_signature_crime'],data_new['position_id'],data_new['createby']))
 
         type_action = "ADD"
 
-        sql_log = "INSERT INTO signature_crime_log (name_signature_crime_id,name_signature_crime,surname_signature_crime,position_signature_crime,createby,type_action) VALUES (%s,%s,%s,%s,%s,%s)"
-        cursor3.execute(sql_log,(name_signature_crime_id_last,data_new['name_signature_crime'],data_new['surname_signature_crime'],data_new['position_signature_crime'],data_new['createby'],type_action))
+        sql_log = "INSERT INTO signature_crime_log (employeeid,name_signature_crime_id,name_signature_crime,surname_signature_crime,position_id,createby,type_action) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+        cursor.execute(sql_log,(data_new['employeeid'],name_signature_crime_id_last,data_new['name_signature_crime'],data_new['surname_signature_crime'],data_new['position_id'],data_new['createby'],type_action))
         return "success"
     except Exception as e:
         logserver(e)
@@ -34,20 +34,20 @@ def EditSignature_crime(cursor):
         source = dataInput['source']
         data_new = source
         sqlQry = "SELECT * FROM signature_crime WHERE name_signature_crime_id=%s"
-        cursor3.execute(sqlQry,data_new['name_signature_crime_id'])
-        columns = [column[0] for column in cursor3.description]
-        result = toJson(cursor3.fetchall(),columns)
+        cursor.execute(sqlQry,data_new['name_signature_crime_id'])
+        columns = [column[0] for column in cursor.description]
+        result = toJson(cursor.fetchall(),columns)
 
         type_action = "Edit"
 
-        sql_log = "INSERT INTO signature_crime_log (name_signature_crime_id,name_signature_crime,surname_signature_crime,position_signature_crime,createby,type_action) VALUES (%s,%s,%s,%s,%s,%s)"
-        cursor3.execute(sql_log,(result[0]['name_signature_crime_id'],result[0]['name_signature_crime'],result[0]['surname_signature_crime'],result[0]['position_signature_crime'],data_new['createby'],type_action))
+        sql_log = "INSERT INTO signature_crime_log (employeeid,name_signature_crime_id,name_signature_crime,surname_signature_crime,position_id,createby,type_action) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+        cursor.execute(sql_log,(result[0]['employeeid'],result[0]['name_signature_crime_id'],result[0]['name_signature_crime'],result[0]['surname_signature_crime'],result[0]['position_id'],data_new['createby'],type_action))
 
         sqlUp = "DELETE FROM signature_crime WHERE name_signature_crime_id=%s"
-        cursor3.execute(sqlUp,(data_new['name_signature_crime_id']))
+        cursor.execute(sqlUp,(data_new['name_signature_crime_id']))
 
-        sql = "INSERT INTO signature_crime (name_signature_crime_id,name_signature_crime,surname_signature_crime,position_signature_crime	,createby) VALUES (%s,%s,%s,%s,%s)"
-        cursor3.execute(sql,(result[0]['name_signature_crime_id'],data_new['name_signature_crime'],data_new['surname_signature_crime'],data_new['position_signature_crime'],data_new['createby']))
+        sql = "INSERT INTO signature_crime (employeeid,name_signature_crime_id,name_signature_crime,surname_signature_crime,position_id,createby) VALUES (%s,%s,%s,%s,%s,%s)"
+        cursor.execute(sql,(result[0]['employeeid'],result[0]['name_signature_crime_id'],data_new['name_signature_crime'],data_new['surname_signature_crime'],data_new['position_id'],data_new['createby']))
         return "success"
     except Exception as e:
         logserver(e)
@@ -56,7 +56,7 @@ def EditSignature_crime(cursor):
 @connect_sql()
 def QrySignature_crime(cursor):
     try:
-        sql = "SELECT name_signature_crime,surname_signature_crime,name_signature_crime_id,position_signature_crime FROM signature_crime"
+        sql = "SELECT signature_crime.employeeid,signature_crime.name_signature_crime_id,signature_crime.name_signature_crime,signature_crime.surname_signature_crime,signature_crime.position_id,position.position_detail FROM signature_crime INNER JOIN position ON signature_crime.position_id = position.position_id"
         cursor.execute(sql)
         columns = [column[0] for column in cursor.description]
         result = toJson(cursor.fetchall(),columns)
@@ -73,20 +73,20 @@ def DeleteSignature_crime():
         source = dataInput['source']
         data_new = source
         sqlQry = "SELECT * FROM signature_crime WHERE name_signature_crime_id=%s"
-        cursor3.execute(sqlQry,data_new['name_signature_crime_id'])
-        columns = [column[0] for column in cursor3.description]
-        result = toJson(cursor3.fetchall(),columns)
+        cursor.execute(sqlQry,data_new['name_signature_crime_id'])
+        columns = [column[0] for column in cursor.description]
+        result = toJson(cursor.fetchall(),columns)
 
         type_action = "Delete"
 
-        sql_log = "INSERT INTO signature_crime_log (name_signature_crime_id,name_signature_crime,surname_signature_crime,position_signature_crime,createby,type_action) VALUES (%s,%s,%s,%s,%s,%s)"
-        cursor3.execute(sql_log,(result[0]['name_signature_crime_id'],result[0]['name_signature_crime'],result[0]['surname_signature_crime'],result[0]['position_signature_crime'],data_new['createby'],type_action))
+        sql_log = "INSERT INTO signature_crime_log (employeeid,name_signature_crime_id,name_signature_crime,surname_signature_crime,position_id,createby,type_action) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+        cursor.execute(sql_log,(result[0]['employeeid'],result[0]['name_signature_crime_id'],result[0]['name_signature_crime'],result[0]['surname_signature_crime'],result[0]['position_id'],data_new['createby'],type_action))
 
         sqlUp = "DELETE FROM signature_crime WHERE name_signature_crime_id=%s"
-        cursor3.execute(sqlUp,(data_new['name_signature_crime_id']))
+        cursor.execute(sqlUp,(data_new['name_signature_crime_id']))
         connection.commit()
         connection.close()
-        return "Success"
+        return "success"
     except Exception as e:
         logserver(e)
         return "fail"
