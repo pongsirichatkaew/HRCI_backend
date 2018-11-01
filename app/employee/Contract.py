f353b95123f3286c4236db276e5fc90c2f51c431
Jirakit_Dan <Black_Sky_light@hotmail.com>
2018-11-01 14:51:33 +0700

    เพิ่มปุ่มกรณีต้องการGENสัญญาจ้างใหม่และรหัสพนักงานใหม่หลังจากกดผ่านสัมภาษณ์

--- app/employee/Contract.py
+++ app/employee/Contract.py
@@ -239,11 +239,11 @@ def QryListContract(cursor):
     try:
         connection = mysql.connect()
         cursor = connection.cursor()
-        sql = "SELECT employee.name_th,employee.employeeid,employee.surname_th,employee.citizenid,company.company_short_name,position.position_detail,section.sect_detail,org_name.org_name_detail,cost_center_name.cost_detail FROM employee INNER JOIN company ON employee.company_id = company.companyid\
-                                      INNER JOIN section ON employee.section_id = section.sect_id\
-                                      INNER JOIN position ON employee.position_id = position.position_id\
-                                      INNER JOIN org_name ON employee.org_name_id = org_name.org_name_id\
-                                      INNER JOIN cost_center_name ON employee.cost_center_name_id = cost_center_name.cost_center_name_id"
+        sql = "SELECT employee.name_th,employee.employeeid,employee.surname_th,employee.citizenid,company.company_short_name,position.position_detail,section.sect_detail,org_name.org_name_detail,cost_center_name.cost_detail FROM employee LEFT JOIN company ON employee.company_id = company.companyid\
+                                      LEFT JOIN position ON employee.position_id = position.position_id\
+                                      LEFT JOIN section ON employee.section_id = section.sect_id\
+                                      LEFT JOIN org_name ON employee.org_name_id = org_name.org_name_id\
+                                      LEFT JOIN cost_center_name ON employee.cost_center_name_id = cost_center_name.cost_center_name_id"
         cursor.execute(sql)
         columns = [column[0] for column in cursor.description]
         result = toJson(cursor.fetchall(),columns)
@@ -280,11 +280,11 @@ def QryListContract_by_mounth(cursor):
         year=str(data_new['year'])
         month=str(data_new['month'])
         companyid=str(data_new['companyid'])
-        sql = """SELECT employee.name_th,employee.employeeid,employee.surname_th,employee.citizenid,company.company_short_name,position.position_detail,section.sect_detail,org_name.org_name_detail,cost_center_name.cost_detail FROM employee INNER JOIN company ON employee.company_id = company.companyid\
-                                      INNER JOIN section ON employee.section_id = section.sect_id\
-                                      INNER JOIN position ON employee.position_id = position.position_id\
-                                      INNER JOIN org_name ON employee.org_name_id = org_name.org_name_id\
-                                      INNER JOIN cost_center_name ON employee.cost_center_name_id = cost_center_name.cost_center_name_id\
+        sql = """SELECT employee.name_th,employee.employeeid,employee.surname_th,employee.citizenid,company.company_short_name,position.position_detail,section.sect_detail,org_name.org_name_detail,cost_center_name.cost_detail FROM employee LEFT JOIN company ON employee.company_id = company.companyid\
+                                      LEFT JOIN section ON employee.section_id = section.sect_id\
+                                      LEFT JOIN position ON employee.position_id = position.position_id\
+                                      LEFT JOIN org_name ON employee.org_name_id = org_name.org_name_id\
+                                      LEFT JOIN cost_center_name ON employee.cost_center_name_id = cost_center_name.cost_center_name_id\
         WHERE employee.start_work LIKE '%""" + month + """-""" + year + """' AND employee.company_id='"""+companyid +"""'"""
         cursor.execute(sql)
         columns = [column[0] for column in cursor.description]
@@ -302,16 +302,154 @@ def QryListContract_by_mounth_new(cursor):
         data_new = source
         year=str(data_new['year'])
         month=str(data_new['month'])
-        sql = """SELECT employee.name_th,employee.employeeid,employee.surname_th,employee.citizenid,company.company_short_name,position.position_detail,section.sect_detail,org_name.org_name_detail,cost_center_name.cost_detail FROM employee INNER JOIN company ON employee.company_id = company.companyid\
-                                      INNER JOIN section ON employee.section_id = section.sect_id\
-                                      INNER JOIN position ON employee.position_id = position.position_id\
-                                      INNER JOIN org_name ON employee.org_name_id = org_name.org_name_id\
-                                      INNER JOIN cost_center_name ON employee.cost_center_name_id = cost_center_name.cost_center_name_id\
+        sql = """SELECT employee.name_th,employee.employeeid,employee.surname_th,employee.citizenid,company.company_short_name,position.position_detail,section.sect_detail,org_name.org_name_detail,cost_center_name.cost_detail FROM employee LEFT JOIN company ON employee.company_id = company.companyid\
+                                      LEFT JOIN section ON employee.section_id = section.sect_id\
+                                      LEFT JOIN position ON employee.position_id = position.position_id\
+                                      LEFT JOIN org_name ON employee.org_name_id = org_name.org_name_id\
+                                      LEFT JOIN cost_center_name ON employee.cost_center_name_id = cost_center_name.cost_center_name_id\
         WHERE employee.start_work LIKE '%""" + month + """-""" + year + """'"""
         cursor.execute(sql)
         columns = [column[0] for column in cursor.description]
         result = toJson(cursor.fetchall(),columns)
         return jsonify(result)
+    except Exception as e:
+        logserver(e)
+        return "fail"
+@app.route('/EditEmployee_Contract', methods=['POST'])
+@connect_sql()
+def EditEmployee_Contract(cursor):
+    try:
+        dataInput = request.json
+        source = dataInput['source']
+        data_new = source
+
+        sql = "SELECT employee.citizenid,employee.company_id,employee.start_work,Contract.contract_id FROM employee INNER JOIN Contract ON employee.citizenid = Contract.ID_CardNo WHERE employeeid=%s"
+        cursor.execute(sql,data_new['employeeid'])
+        columns = [column[0] for column in cursor.description]
+        result = toJson(cursor.fetchall(),columns)
+
+        sql_log = "INSERT INTO Contract_log (employeeid_old,ID_CardNo,contract_id,company_id,createby) VALUES (%s,%s,%s,%s,%s)"
+        cursor.execute(sql_log,(data_new['employeeid'],result[0]['citizenid'],result[0]['contract_id'],result[0]['company_id'],data_new['createby']))
+
+        start_date_ = result[0]['start_work']
+        split_str_date = start_date_.split("-")
+        str_date_year = split_str_date[2]
+
+        now_contract = str_date_year
+        date_contract = str(int(now_contract)+543)
+        date_sub_contract = date_contract[2:]
+        try:
+            sql_contract_id = "SELECT contract_id,year FROM Contract WHERE companyid=%s AND validstatus =1 AND year=%s ORDER BY contract_id DESC LIMIT 1"
+            cursor.execute(sql_contract_id,(data_new['company_id'],date_contract))
+            columns = [column[0] for column in cursor.description]
+            resultsql_contract_id = toJson(cursor.fetchall(),columns)
+            year_contract = resultsql_contract_id[0]['year']
+            contract_id_ = resultsql_contract_id[0]['contract_id']
+            year_sub_con = year_contract[2:]
+            if year_sub_con==date_sub_contract:
+                contract_id_ = resultsql_contract_id[0]['contract_id']
+            else:
+                contract_id_ = 0
+        except Exception as e:
+            contract_id_ = 0
+        contract_id_last = int(contract_id_)+1
+
+        sqlUp_EmerContact = "UPDATE Contract SET contract_id=%s,company_id=%s WHERE ID_CardNo=%s"
+        cursor.execute(sqlUp_EmerContact,(contract_id_last,data_new['company_id'],result[0]['citizenid']))
+
+        companyid__ = int(data_new['company_id'])
+        if companyid__==1:
+            now = str_date_year
+            date_n = str(int(now))
+            form_employee = date_n
+        elif companyid__==21:
+            now = str_date_year
+            date_n = str(int(now))
+            form_employee = date_n[2:]
+        else:
+            now = str_date_year
+            date_n = str(int(now)+543)
+            form_employee = date_n[2:]
+        sqlcompafirst = "SELECT acronym FROM company WHERE companyid=%s AND validstatus=1"
+        cursor.execute(sqlcompafirst,data_new['company_id'])
+        columnscompafirst = [column[0] for column in cursor.description]
+        resultcompafirst = toJson(cursor.fetchall(),columnscompafirst)
+        coun_length =len(resultcompafirst[0]['acronym'])
+        coun_company = str(resultcompafirst[0]['acronym'])
+        try:
+            sqlEmployee = "SELECT employeeid FROM employee WHERE company_id=%s ORDER BY employeeid DESC LIMIT 1"
+            cursor.execute(sqlEmployee,data_new['company_id'])
+            columnsEmployee = [column[0] for column in cursor.description]
+            resultEmployee = toJson(cursor.fetchall(),columnsEmployee)
+            Emp_last = resultEmployee[0]['employeeid']
+            if companyid__!=1:
+                form_employee2 = Emp_last[coun_length:]
+                form_employee3 = form_employee2[:-3]
+                if form_employee3==form_employee:
+                    Emp_last = resultEmployee[0]['employeeid']
+                else:
+                    Emp_last = coun_company+"000"
+            else:
+                form_employee2 = Emp_last[coun_length:]
+                form_employee3 = form_employee2[:-4]
+                if form_employee3==form_employee:
+                    Emp_last = resultEmployee[0]['employeeid']
+                else:
+                    Emp_last = coun_company+"000"
+        except Exception as e:
+            if companyid__!=1:
+                Emp_last = coun_company+"000"
+            else:
+               Emp_last = coun_company+"000"
+        type = Emp_last
+        if  coun_length==0:
+            coun_length=2
+        elif coun_length==1:
+            coun_length=3
+        else:
+            coun_length=coun_length+2
+        if companyid__!=1:
+            codelast = int(str(type[coun_length:]))+1
+        else:
+            codelast = str(int(str(type[coun_length:]))+1)
+            codelast = codelast[2:]
+            if codelast=="":
+               codelast=1
+            else:
+                codelast=codelast
+        if companyid__!=1:
+            if  codelast<=9:
+                codelast=str(codelast)
+                codesumlast="00"+codelast
+            elif codelast<=99:
+                codelast=str(codelast)
+                codesumlast="0"+codelast
+            else:
+                codesumlast=str(codelast)
+        else:
+            if  codelast<=9:
+                codelast=str(codelast)
+                codesumlast="000"+codelast
+            elif codelast<=99:
+                codelast=str(codelast)
+                codesumlast="00"+codelast
+            elif codelast<=999:
+                codelast=str(codelast)
+                codesumlast="0"+codelast
+            else:
+                codesumlast=str(codelast)
+        first_character = resultcompafirst[0]['acronym']
+        if companyid__==1:
+            employeeid = first_character+form_employee+codesumlast
+        elif companyid__==21:
+            employeeid = first_character+form_employee+codesumlast
+        else:
+            employeeid = first_character+form_employee+codesumlast
+
+        sqlUp_Emp_ = "UPDATE employee SET employeeid=%s,company_id=%s WHERE citizenid=%s"
+        cursor.execute(sqlUp_Emp_,(employeeid,data_new['company_id'],result[0]['citizenid']))
+
+        return "Success"
     except Exception as e:
         logserver(e)
         return "fail"
