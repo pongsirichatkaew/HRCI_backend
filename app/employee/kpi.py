dde66a60193dc2cf721bd9c1ed2ffefe14ab0ca5
rainbow <bowbeau32@gmail.com>
2018-12-21 19:59:48 +0700

    fixbug

--- app/employee/kpi.py
+++ app/employee/kpi.py
@@ -339,64 +339,64 @@ def Add_board_kpi_v2(cursor):
 @app.route('/Add_board_kpi_no_result', methods=['POST'])
 @connect_sql()
 def Add_board_kpi_no_result(cursor):
-    # try:
-    dataInput = request.json
-    source = dataInput['source']
-    data_new = source
-    employeeid = data_new['employeeid_board']
-
-    nameKpi__ = str(data_new['name_kpi'])+" "+str(data_new['surname_kpi'])
-
-    sql_be = "INSERT INTO board_kpi_v2(employeeid_board,name,group_kpi,createby) VALUES (%s,%s,%s,%s)"
-    cursor.execute(sql_be,(data_new['employeeid_board'],nameKpi__,data_new['group_kpi_id'],data_new['createby']))
-
-    sql = "INSERT INTO Admin (employeeid,username,name,permission,createby) VALUES (%s,%s,%s,%s,%s)"
-    cursor.execute(sql,(data_new['employeeid_board'],data_new['username'],nameKpi__,data_new['group_kpi_id'],data_new['createby']))
-
-    group_kpi_id = ""
     try:
         dataInput = request.json
         source = dataInput['source']
         data_new = source
-        group_ = str(data_new['group_kpi_id'])
-        group_kpi_id = 'WHERE group_kpi='+'"'+group_+'"'
+        employeeid = data_new['employeeid_board']
+
+        nameKpi__ = str(data_new['name_kpi'])+" "+str(data_new['surname_kpi'])
+
+        sql_be = "INSERT INTO board_kpi_v2(employeeid_board,name,group_kpi,createby) VALUES (%s,%s,%s,%s)"
+        cursor.execute(sql_be,(data_new['employeeid_board'],nameKpi__,data_new['group_kpi_id'],data_new['createby']))
+
+        sql = "INSERT INTO Admin (employeeid,username,name,permission,createby) VALUES (%s,%s,%s,%s,%s)"
+        cursor.execute(sql,(data_new['employeeid_board'],data_new['username'],nameKpi__,data_new['group_kpi_id'],data_new['createby']))
+
+        group_kpi_id = ""
+        try:
+            dataInput = request.json
+            source = dataInput['source']
+            data_new = source
+            group_ = str(data_new['group_kpi_id'])
+            group_kpi_id = 'WHERE group_kpi='+'"'+group_+'"'
+        except Exception as e:
+            pass
+        try:
+            dataInput = request.json
+            source = dataInput['source']
+            data_new = source
+            group_2 = str(data_new['group_kpi_id2'])
+            group_kpi_id = 'WHERE group_kpi IN ('+'"'+group_+'"'+','+'"'+group_2+'"'+')'
+        except Exception as e:
+            pass
+        try:
+            sql_emp_kpi = "SELECT employee_kpi.employeeid FROM employee_kpi INNER JOIN board_kpi ON employee_kpi.employeeid = board_kpi.employeeid "+group_kpi_id+" AND board_kpi.grade_board=Null "
+            cursor.execute(sql_emp_kpi)
+            columns = [column[0] for column in cursor.description]
+            result = toJson(cursor.fetchall(),columns)
+            check_emid = result[0]['employeeid']
+        except Exception as e:
+            sql_emp_kpi = "SELECT employeeid FROM employee_kpi "+group_kpi_id+" "
+            cursor.execute(sql_emp_kpi)
+            columns = [column[0] for column in cursor.description]
+            result = toJson(cursor.fetchall(),columns)
+
+
+        type_action = "ADD"
+
+        for i in xrange(len(result)):
+            sqlIn_be = "INSERT INTO board_kpi(employeeid,employeeid_board,name_kpi,surname_kpi,position_kpi,createby) VALUES (%s,%s,%s,%s,%s,%s)"
+            cursor.execute(sqlIn_be,(result[i]['employeeid'],data_new['employeeid_board'],data_new['name_kpi'],data_new['surname_kpi'],data_new['position_kpi'],data_new['createby']))
+
+        for i in xrange(len(result)):
+            sqlIn_be = "INSERT INTO board_kpi_log(employeeid,employeeid_board,name_kpi,surname_kpi,position_kpi,createby,type_action) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+            cursor.execute(sqlIn_be,(result[i]['employeeid'],data_new['employeeid_board'],data_new['name_kpi'],data_new['surname_kpi'],data_new['position_kpi'],data_new['createby'],type_action))
+
+        return "Success"
     except Exception as e:
-        pass
-    try:
-        dataInput = request.json
-        source = dataInput['source']
-        data_new = source
-        group_2 = str(data_new['group_kpi_id2'])
-        group_kpi_id = 'WHERE group_kpi IN ('+'"'+group_+'"'+','+'"'+group_2+'"'+')'
-    except Exception as e:
-        pass
-    try:
-        sql_emp_kpi = "SELECT employee_kpi.employeeid FROM employee_kpi INNER JOIN board_kpi ON employee_kpi.employeeid = board_kpi.employeeid "+group_kpi_id+" AND board_kpi.grade_board=Null "
-        cursor.execute(sql_emp_kpi)
-        columns = [column[0] for column in cursor.description]
-        result = toJson(cursor.fetchall(),columns)
-        check_emid = result[0]['employeeid']
-    except Exception as e:
-        sql_emp_kpi = "SELECT employeeid FROM employee_kpi "+group_kpi_id+" "
-        cursor.execute(sql_emp_kpi)
-        columns = [column[0] for column in cursor.description]
-        result = toJson(cursor.fetchall(),columns)
-
-
-    type_action = "ADD"
-
-    for i in xrange(len(result)):
-        sqlIn_be = "INSERT INTO board_kpi(employeeid,employeeid_board,name_kpi,surname_kpi,position_kpi,createby) VALUES (%s,%s,%s,%s,%s,%s)"
-        cursor.execute(sqlIn_be,(result[i]['employeeid'],data_new['employeeid_board'],data_new['name_kpi'],data_new['surname_kpi'],data_new['position_kpi'],data_new['createby']))
-
-    for i in xrange(len(result)):
-        sqlIn_be = "INSERT INTO board_kpi_log(employeeid,employeeid_board,name_kpi,surname_kpi,position_kpi,createby,type_action) VALUES (%s,%s,%s,%s,%s,%s,%s)"
-        cursor.execute(sqlIn_be,(result[i]['employeeid'],data_new['employeeid_board'],data_new['name_kpi'],data_new['surname_kpi'],data_new['position_kpi'],data_new['createby'],type_action))
-
-    return "Success"
-    # except Exception as e:
-    #     logserver(e)
-    #     return "fail"
+        logserver(e)
+        return "fail"
 @app.route('/Delete_board_kpi_no_result', methods=['POST'])
 @connect_sql()
 def Delete_board_kpi_no_result(cursor):
