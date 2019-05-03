2ec2b955f83099f0675d86d6c01e82c2cfee6d5c
Jirakit_Dan <Black_Sky_light@hotmail.com>
2019-05-03 12:40:47 +0700

    teeee

--- app/employee/kpi.py
+++ app/employee/kpi.py
@@ -959,7 +959,7 @@ def Export_kpi(cursor):
 @app.route('/Export_kpi_hr', methods=['POST'])
 @connect_sql()
 def Export_kpi_hr(cursor):
-    try:
+    # try:
         dataInput = request.json
         source = dataInput['source']
         data_new = source
@@ -1054,11 +1054,10 @@ def Export_kpi_hr(cursor):
         os.remove(filename_tmp)
         displayColumns = ['isSuccess','reasonCode','reasonText','excel_base64']
         displayData = [(isSuccess,reasonCode,reasonText,encoded_string)]
-        print(success)
         return jsonify(toDict(displayData,displayColumns))
-    except Exception as e:
-        logserver(e)
-        return "fail"
+    # except Exception as e:
+    #     logserver(e)
+    #     return "fail"
 @app.route('/userGetKpiFile/<path>', methods=['GET'])
 def userGetKpiFile(path):
     return send_from_directory('../uploads/', path)
