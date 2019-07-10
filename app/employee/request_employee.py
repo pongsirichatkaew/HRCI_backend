da1c39289818516f8a2eecc092ab4048c7846859
LAPTOP-SP0D1800\GAPP <gab2538@gmail.com>
2019-07-10 17:21:37 +0700

    Education(for user)

--- app/employee/request_employee.py
+++ app/employee/request_employee.py
@@ -1085,9 +1085,10 @@ def QryEmp_request_leader():
                 pass
             connection = mysql.connect()
             cursor = connection.cursor()
-            sql = "SELECT employee.name_th,employee.employeeid,employee.surname_th,employee.citizenid,employee.start_work,employee.EndWork_probation,employee.EmploymentAppNo,company.company_short_name,position.position_detail,section.sect_detail,org_name.org_name_detail,cost_center_name.cost_detail,status_request.status_detail,status_request.path_color,status_request.font_color,approve_request.tier_approve FROM employee LEFT JOIN company ON company.companyid = employee.company_id\
+            sql = "SELECT employee.name_th,employee.employeeid,employee.surname_th,Education.institute,Education.qualification,Education.major,employee.citizenid,employee.start_work,employee.EndWork_probation,employee.EmploymentAppNo,company.company_short_name,position.position_detail,section.sect_detail,org_name.org_name_detail,cost_center_name.cost_detail,status_request.status_detail,status_request.path_color,status_request.font_color,approve_request.tier_approve FROM employee LEFT JOIN company ON company.companyid = employee.company_id\
                                           LEFT JOIN position ON position.position_id = employee.position_id\
                                           LEFT JOIN section ON section.sect_id = employee.section_id\
+                                          LEFT JOIN Education ON Education.ID_CardNo = employee.citizenid\
                                           LEFT JOIN org_name ON org_name.org_name_id = employee.org_name_id\
                                           LEFT JOIN cost_center_name ON cost_center_name.cost_center_name_id = employee.cost_center_name_id\
                                           LEFT JOIN approve_request ON approve_request.employeeid = employee.employeeid\
